86c94be8c0f3273e600b58c7436f3e2319f930eb
Smart-SangGe <2251250136@qq.com>
2023-06-11 23:24:14 +0800

    new file:   edge.py

---
+++ edge.py
@@ -0,0 +1,57 @@
+UR = 0
+UF = 1
+UL = 2
+UB = 3
+DR = 4
+DF = 5
+DL = 6
+DB = 7
+FR = 8
+FL = 9
+BL = 10
+BR = 11
+
+edge_values = (
+    UR,
+    UF,
+    UL,
+    UB,
+    DR,
+    DF,
+    DL,
+    DB,
+    FR,
+    FL,
+    BL,
+    BR,
+)
+
+edge_keys = (
+    'UR',
+    'UF',
+    'UL',
+    'UB',
+    'DR',
+    'DF',
+    'DL',
+    'DB',
+    'FR',
+    'FL',
+    'BL',
+    'BR',
+)
+
+edge = {
+    'UR': 'UR',
+    'UF': 'UF',
+    'UL': 'UL',
+    'UB': 'UB',
+    'DR': 'DR',
+    'DF': 'DF',
+    'DL': 'DL',
+    'DB': 'DB',
+    'FR': 'FR',
+    'FL': 'FL',
+    'BL': 'BL',
+    'BR': 'BR'
+}
