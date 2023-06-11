fd4bb7c6c616f7d00331ecfc1b978c8cb02bd426
Smart-SangGe <2251250136@qq.com>
2023-06-11 23:23:42 +0800

    new file:   corner.py

---
+++ corner.py
@@ -0,0 +1,45 @@
+"""
+角块
+    """
+
+URF = 0
+UFL = 1
+ULB = 2
+UBR = 3
+DFR = 4
+DLF = 5
+DBL = 6
+DRB = 7
+
+corner_values = (
+    URF,
+    UFL,
+    ULB,
+    UBR,
+    DFR,
+    DLF,
+    DBL,
+    DRB,
+)
+
+corner_keys = (
+    'URF',
+    'UFL',
+    'ULB',
+    'UBR',
+    'DFR',
+    'DLF',
+    'DBL',
+    'DRB',
+)
+
+corner = {
+    'URF': 'URF',
+    'UFL': 'UFL',
+    'ULB': 'ULB',
+    'UBR': 'UBR',
+    'DFR': 'DFR',
+    'DLF': 'DLF',
+    'DBL': 'DBL',
+    'DRB': 'DRB'
+}
